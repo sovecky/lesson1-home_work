699010edf7c572d5b0a9e157795ab0247fba7c19
sovecky <36189625+sovecky@users.noreply.github.com>
2019-07-02 19:27:54 +0300

    Delete 2.2.3.py

--- 2.2.3.py
+++
@@ -1,7 +0,0 @@
-import random
-
-n = int(input('введите количество случайных элементов в списке: '))
-my_list = []
-for el in range(n):
-    my_list.append(random.randint(-100, 100))
-print(my_list)
