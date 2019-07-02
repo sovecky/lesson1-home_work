025c864bccabe4605c7611d01964bab64f33569e
Evgeny <Sovecky@yandex.ru>
2019-07-02 20:31:51 +0400

    Initial commit

--- 2.2.1.py
+++
@@ -1,9 +0,0 @@
-import math;
-
-list = [2, -5, 8, 9, -25, 25, 4]
-list2 = []
-for item in list:
-    if item > 0 and math.sqrt(item) % 1 == 0:
-        list2.append(int(math.sqrt(item)))
-print(list2)
-
