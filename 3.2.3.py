9b553f89154c71be019fc3e45c03e7b3eb4097af
Evgeny <Sovecky@yandex.ru>
2019-07-05 00:50:58 +0400

    Initial commit

--- 3.2.3.py
+++
@@ -1,6 +0,0 @@
-def filter_func(f, m):
-    return (item for item in m if f(item))
-
-
-print(list(filter_func(lambda x: True if x > 3  else False,
-                       [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])))
